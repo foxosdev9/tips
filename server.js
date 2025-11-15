70504229579e89686240be5bad2cca6f116ff4ea
foxosdev9 <chentoufanass3@gmail.com>
2025-11-15 18:31:48 +0100

    fix server problem v1

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 require('dotenv').config({path: './config.env'});
-const http = require('http');
+// const http = require('http');
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
 mongoose
-.connect(process.env.MONGODB_Atlas)
+.connect(process.env.MONGO_URI)
 .then(msg => console.log('DATABASE CONNECTED '))
 .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server run at : ${PORT}`)
 });
